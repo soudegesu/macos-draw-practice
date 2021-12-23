863a62c577a9a56e1503b57396b5c2e475c6783d
soudegesu <soudegesu@hotmail.co.jp>
2021-12-23 17:03:04 +0900

    時間経過で消える

--- macos-draw-practice/OverlayView.swift
+++ macos-draw-practice/OverlayView.swift
@@ -9,19 +9,21 @@ import SwiftUI
 
 enum DrawType {
     case red
-    case clear
     case black
     
     var color: Color {
         switch self {
             case .red:
                 return Color.red
-            case .clear:
-                return Color.white
             case .black:
                 return Color.black
         }
     }
+}
+
+struct Line: Identifiable {
+    var drawPoints: [DrawPoints] = []
+    var id = UUID()
 }
 
 struct DrawPoints: Identifiable {
@@ -30,74 +32,57 @@ struct DrawPoints: Identifiable {
     var id = UUID()
 }
 
-struct DrawPathView: View {
-    var drawPointsArray: [DrawPoints]
-
-    var body: some View {
-        ZStack {
-            ForEach(drawPointsArray) { data in
-                Path { path in
-                    path.addLines(data.points)
-                }
-                .stroke(data.color, lineWidth: 10)
-            }
-        }
-    }
-}
-
 struct OverlayView: View {
-    @State var tmpDrawPoints: DrawPoints = DrawPoints(points: [], color: .red)
-    @State var endedDrawPoints: [DrawPoints] = []
+    @State var tmpDrawPoints: DrawPoints = DrawPoints(points: [], color: .black)
+    @State var lines: [Line] = []
     @State var startPoint: CGPoint = CGPoint.zero
-    @State var selectedColor: DrawType = .red
+    @State var selectedColor: DrawType = .black
+    let lineWidth = CGFloat(10.0)
 
+    @ViewBuilder
     var body: some View {
         VStack {
             Rectangle()
                 .foregroundColor(Color.white)
                 .frame(width: 300, height: 300, alignment: .center)
                 .overlay(
-                    DrawPathView(drawPointsArray: endedDrawPoints)
-                        .overlay(
-                            // ドラッグ中の描画。指を離したらここの描画は消えるがDrawPathViewが上書きするので見た目は問題ない
-                            Path { path in
-                                path.addLines(self.tmpDrawPoints.points)
-                            }
-                            .stroke(self.tmpDrawPoints.color, lineWidth: 10)
-                        )
+                  ZStack {
+                    ForEach(lines) { line in
+                      ZStack {
+                        ForEach(line.drawPoints) { data in
+                          Path { path in
+                              path.addLines(data.points)
+                          }
+                          .stroke(Color.red, lineWidth: lineWidth)
+                        }
+                      }
+                    }
+                  }
+                  .overlay(
+                    Path { path in
+                        path.addLines(self.tmpDrawPoints.points)
+                    }
+                    .stroke(self.tmpDrawPoints.color, lineWidth: lineWidth)
+                  )
                 )
                 .gesture(
                     DragGesture()
                         .onChanged({ (value) in
-                            if self.startPoint != value.startLocation {
-                              self.tmpDrawPoints.points.append(value.location)
-                              self.tmpDrawPoints.color = self.selectedColor.color
-                            }
-                            self.endedDrawPoints.append(self.tmpDrawPoints)
+//                          if self.startPoint != value.startLocation || self.startPoint == .zero {
+                            self.tmpDrawPoints.points.append(value.location)
+                            self.tmpDrawPoints.color = self.selectedColor.color
+//                          }
                         })
                         .onEnded({ (value) in
-                            self.startPoint = value.startLocation
-                            self.endedDrawPoints.append(self.tmpDrawPoints)
-                            self.tmpDrawPoints = DrawPoints(points: [], color: self.selectedColor.color)
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-                              self.endedDrawPoints = []
-                            }
+                          self.startPoint = value.startLocation
+                          let newLine = Line(drawPoints: [self.tmpDrawPoints])
+                          self.lines.append(newLine)
+                          DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                            self.lines = self.lines.filter { $0.id != newLine.id }
+                          }
+                          self.tmpDrawPoints.points = []
                         })
                 )
-            VStack(spacing: 10) {
-                Button(action: {
-                    self.selectedColor = .red
-
-                }) { Text("赤")
-                }
-                Button(action: {
-                    self.selectedColor = .clear
-                }) { Text("消しゴム")
-                }
-                Spacer()
-            }
-            .frame(minWidth: 0.0, maxWidth: CGFloat.infinity)
-            .background(Color.gray)
         }
     }
 }
